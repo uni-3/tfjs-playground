a58867e0ab7282a9941ce737570c53b69f592b02
uni <7pluto8@gmail.com>
2018-08-29 03:31:55 +0900

    add wordcloud component

---
+++ src/components/Wordcloud/Wordcloud.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+import WordCloud from 'react-d3-cloud'
+
+export default class Wordcloud extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  shouldComponentUpdate(nextProps) {
+    let data = this.props.data
+
+    if (JSON.stringify(nextProps.data) === JSON.stringify(data)) {
+      console.log('next false', nextProps.data)
+
+      return false
+    }
+    return true
+  }
+
+  render() {
+    let data = this.props.data
+    if (data === null) {
+      return null
+    }
+    const fontSizeMapper = word => Math.log2(word.value) * 20
+    //const rotate = word => word.value % 360
+    const rotate = word => word.value % 45
+
+    return (
+      <WordCloud
+        data={data}
+        fontSizeMapper={fontSizeMapper}
+        rotate={rotate}
+      />
+
+    )
+  }
+}
